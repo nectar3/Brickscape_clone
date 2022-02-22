f512e99265ff2f9ca9a74d252016fb027f7364cc
amplet7 <amplet7@gmail.com>
2022-02-23 00:42:15 +0900

    드래그 빨리할때 벽 뚫는 문제 해결(using ray)
    dd

--- Assets/Block.cs
+++ Assets/Block.cs
@@ -24,7 +24,6 @@ public class Block : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHan
     private GameObject rayPoint_max;
     private GameObject rayPoint_min;
 
-    Vector3 d; // 이동할 델타
 
     private void Start()
     {
@@ -33,9 +32,9 @@ public class Block : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHan
             dir == Direction.y ? Vector3.up :
             dir == Direction.z ? Vector3.forward : Vector3.zero;
 
+        var dd = Vector3.Dot(new Vector3(0.0f, 0.0f, 0.0f), new Vector3(0.0f, 1.0f, 0.0f));
+        Debug.Log("dd = " + dd);
         blockSize = Mathf.RoundToInt(Vector3.Dot(dirVec, transform.localScale));
-
-        d = Vector3.zero;
 
         // 블럭 끝부분에 레이캐스트 포인트
         var rayPoint = new GameObject("rayPoint_max");
@@ -47,6 +46,8 @@ public class Block : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHan
         rayPoint.transform.SetParent(this.transform);
         rayPoint.transform.localPosition = -dirVec / 2;
         rayPoint_min = rayPoint;
+
+
     }
 
     public void OnBeginDrag(PointerEventData eventData)
@@ -59,23 +60,23 @@ public class Block : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHan
     bool isShaking = false;
     public void ShakeBlock(Vector3 hitPoint, float hitDelta)
     {
-        if (isShaking)
-            return;
-        Debug.Log("hitPoint = " + hitPoint);
+        //if (isShaking)
+        //    return;
+        //Debug.Log("hitPoint = " + hitPoint);
 
-        isShaking = true;
+        //isShaking = true;
 
-        if (Vector3.Dot( transform.position, dirVec) < Vector3.Dot( hitPoint, dirVec)) // 블럭 방향의 성분만 가져오기
-        {
-            Debug.Log("방향 true");
-            transform.DORotate(dirVec * 20, 1f);
-        }
-        else
-        {
-            Debug.Log("방향 false");
-            transform.DORotate(dirVec * -20, 1f);
+        //if (Vector3.Dot( transform.position, dirVec) < Vector3.Dot( hitPoint, dirVec)) // 블럭 방향의 성분만 가져오기
+        //{
+        //    Debug.Log("방향 true");
+        //    transform.DORotate(dirVec * 20, 1f);
+        //}
+        //else
+        //{
+        //    Debug.Log("방향 false");
+        //    transform.DORotate(dirVec * -20, 1f);
 
-        }
+        //}
     }
 
     // TODO: 상대방에게 맞은 hitPoint 에 따라 회전 축 정하기
@@ -94,32 +95,40 @@ public class Block : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHan
             dir == Direction.x ? -eventData.delta.x :
             dir == Direction.y ? eventData.delta.y :
             dir == Direction.z ? eventData.delta.x : 0;
-        var delta_clamped = Mathf.Clamp(delta, -3, 3);
+        delta = Mathf.Clamp(delta, -20, 20);
 
-        d = dirVec * delta_clamped * dragSpeed * Time.deltaTime;
-        if (Physics.Raycast(rayPoint_max.transform.position, dirVec, out RaycastHit hit, 1f))
+        var d = dirVec * delta * dragSpeed * Time.deltaTime;
+        if (delta > 0)
         {
-            if (delta_clamped > 0 && hit.distance < 0.1f)
+            if (Physics.Raycast(rayPoint_max.transform.position, dirVec, out RaycastHit hit, 4f))
             {
-                d = Vector3.zero;
-                var otherBlock = hit.collider.gameObject.GetComponent<Block>();
-                if (otherBlock)
-                    otherBlock.ShakeBlock(hit.point, delta);
+                var amount = Vector3.Dot(d, dirVec);
+                var amount_min = Mathf.Min(amount, hit.distance - 0.1f);
+                d = dirVec * amount_min;
+                if (hit.distance < 0.1f)
+                    d = Vector3.zero;
             }
         }
-        if(Physics.Raycast(rayPoint_min.transform.position, -dirVec, out hit, 1f))
+        else if (delta < 0)
         {
-            if (delta_clamped < 0 && hit.distance < 0.1f)
+            if (Physics.Raycast(rayPoint_max.transform.position, -dirVec, out RaycastHit hit, 4f))
             {
-                d = Vector3.zero;
-                var otherBlock = hit.collider.gameObject.GetComponent<Block>();
-                if (otherBlock)
-                    otherBlock.ShakeBlock(hit.point, delta);
+                var amount = Vector3.Dot(d, dirVec);
+                var amount_min = Mathf.Min(Mathf.Abs(amount), hit.distance - 0.1f);
+                d = dirVec * -amount_min;
+                if (hit.distance < 0.1f)
+                    d = Vector3.zero;
+
             }
         }
+        else
+            d = Vector3.zero;
+
         var newPos = transform.position + d;
         transform.position = newPos;
     }
+
+
 
 
     public void OnEndDrag(PointerEventData eventData)
